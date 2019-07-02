59c63a0f8bbf0378e3f59a10800b6d2a61876ace
Fthi-S3074641 <fthialem@gmail.com>
2019-07-02 15:57:22 +0300

    =index.js for webpack

--- config/index.js
+++ config/index.js
@@ -65,5 +65,8 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
-  }
+  },
+  loaders: [
+    { test: /\.css$/, loader: "css-loader" }
+  ]
 }
